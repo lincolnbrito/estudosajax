07b9f8af7ef001bfdb517c6373f117456637b9f8
Lincoln Brito <lincoln.sbrito@gmail.com>
2012-02-02 18:06:07 -0200

    alterando resultado na Div se nao retornar dados

--- auto_complete/autoCompletar.php
+++ auto_complete/autoCompletar.php
@@ -16,18 +16,25 @@
 	$p[] = "gato";
 	$p[] = "porta";
 	
-	$search = $_GET['s'];
+	$item = '';
 	
+	$search = $_GET['s'];
 	$res = "{'pal':[";	
-	for($x=0; $x<11; $x++){
+	
+	for($x=0; $x<count($p); $x++){
 		$sub = substr($p[$x], 0, strlen($search));
+		
 		if($search === $sub){
-			$res .= "'".$p[$x]."',";		
+			$item .= "'".$p[$x]."',";		
 		}
+	}
+	
+	$item = substr($item, 0, (strlen($item)-1));
+	if(strlen($item)>0){
+		$res .= $item."]}";		
+	}else{
+		$res = "{'pal':[]}";
 	}	
-	$res = substr($res, 0, (strlen($res)-1));	
-	$res .= "]}";
 	
 	echo $res;
-	
 ?>
